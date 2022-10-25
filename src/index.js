2d082b15c236de127565f9388abd6d0d75fd0842
Olga greyskop <o.greyskop@gmail.com>
2022-10-25 22:24:32 +0300

    audio and point

--- src/index.js
+++ src/index.js
@@ -3,22 +3,47 @@ const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
  const mainClass = document.querySelector(".main-class");
+ const detailsContainer = document.querySelector(".details-container");
+ const audio = document.querySelector(".audio");
  const HIDDEN = "hidden";
+ const IS_POINT = "is-point";
 function setDetails(anchor){
 detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
 detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
+pauseAudio(1);
+audio.setAttribute('src', anchor.getAttribute('data-details-audio'));
 }
 for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function(){ 
         console.log("event -click on", anchors[i]);
         showDetails();
          setDetails(anchors[i]);
+        
+        playAudio();
+        pause (5000);
     })
-
 }
+function playAudio() {
+    setTimeout(function () {
+        audio.play();
+
+    },1);
+}
+    function pauseAudio(msecs){
+        setTimeout(function (){
+            audio.pause();
+        }, msecs);
+    }
+
 function showDetails(){
     mainClass.classList.remove(HIDDEN);
+    detailsContainer.classList.add(IS_POINT);
+    setTimeout(function(){
+        detailsContainer.classList.remove(IS_POINT)
+    },1)
+
 }
 function hideDetails (){
 mainClass.classList.add(HIDDEN);
+pauseAudio();
 }
