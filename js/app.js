e524f08a973135c31f486c2176d73d0101a8d10d
Olga greyskop <o.greyskop@gmail.com>
2022-11-15 19:42:51 +0200

    homework15 prefix

--- js/app.js
+++ js/app.js
@@ -1,34 +1,14 @@
 /*
-1.	Write function minMax(numbers) 
- 
-1.1.	Takes array of numbers
-1.2.	Returns array containing two numbers: first is minimal value , second is maximal value form input array
-1.3.	Example: minMax([1, 2, 3, 4, 5]) returns array [1, 5]
-1.4.	Implementation Requirements
-1.4.1.	Apply the reduce pattern for getting result array by one “reduce” method 
+2.	Write function deleteWithPrefix(strings, prefix)
+2.1.	Takes array of strings and a prefix value
+2.2.	Returns array containing the strings from the input array without strings starting with a given prefix
+2.3.	Example: deleteWithPrefix([“abc”, “old_abc”, “lmn”, “123”, “old_lmn”], “old_”) returns array [“abc”, “lmn”, “123”]
 */
-let numbers = [3, -25, 10, -12, 7, 2];
-function minMax(numbers) {
-    return numbers.reduce((mm, element) => {
-        if(element < mm[0]) {
-            mm[0] = element;
-        }
-        if(element > mm[1]) {
-            mm[1] = element;
-        }
-        return mm;
-    }, [numbers[0],numbers[0]]);
-}
-console.log(`minMax. input: ${numbers} output: ${minMax(numbers)}`);
+let cities = ['usDetroit', 'isHaifa', 'usChicago', 'isJerusalem'];
+let prefix = 'is';
 
-// let zzz = numbers.map(element => element+1);
-// function xxx() {
-// let outArr = [];
-// for(let i=0; i<numbers.length; i++) {
-//     outArr[i] = numbers[i]+1;
-// }
-// return outArr;
-// }
-// let yyy = xxx();
-// console.log('yyy=', yyy);
-// console.log('zzz=', zzz);
+function deleteWithPrefix(cities, prefix) {
+    return cities.filter(element => !element.startsWith(prefix));
+}
+
+console.log(`deleteWithPrefix. input: ${cities}, prefix: ${prefix} output: ${deleteWithPrefix(cities, prefix)}`);
