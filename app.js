b277ce2da48faedd4dfd71edcf1a8d4f4f933e86
Olga greyskop <o.greyskop@gmail.com>
2022-12-05 21:32:41 +0200

    rectangle this.

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-console.log(anagramm('cat', 'tac'));// true
-console.log(anagramm('good', 'dog'));//false
-console.log(anagramm('look','koll'))//false
-function anagramm(str1,str2){
-    if (str1.length !=str2.length)
-    return false;
-
-    let sorted1 =str1.split('').sort().join('').toLowerCase();
-    let sorted2 =str2.split('').sort().join('').toLowerCase();
-    return (sorted1===sorted2);
+function f1 (){
+    const a=this;
 }
-
+const f2=()=>{
+const a =this;
+return a;
+}
+ const f=f1;
+ f();
+ f2();
+ f1.d=20;
+ console.log(f1.d);
+const rectangle={
+    width:3,
+    height:5,
+    
+}
